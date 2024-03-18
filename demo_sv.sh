c62f72424d9de77641d576925126cb5ebfc2220b
ncpa <ncpa@go.olemiss.edu>
2024-03-18 19:30:55 +0000

    remove wait in the bash script

--- demo_sv.sh
+++ demo_sv.sh
@@ -7,10 +7,7 @@ mkfifo /tmp/main_dection
 mkfifo /tmp/noah_angle
 
 $HOME/BASS_stand_demo_revision/GPIO_stepper/stepper &
-wait
 $HOME/BASS_stand_demo_revision/main/build/src/demo_main -d /dev/ttyACM0 &
-wait
 $HOME/BASS_stand_demo_revision/main/build/src/demo_detection &
-wait
 $HOME/BASS_stand_demo_revision/FIFO_DOA/dir_of_arr &
 
